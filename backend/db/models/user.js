17daa00571b9b1f07a0544439fdc106d65c321e6
Kyle Mooney <kjmooney27@gmail.com>
2023-08-04 13:17:06 -0400

    created Spot migration, model, & seeds

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
     }
     static associate(models) {
       // define association here
+      User.hasMany(models.Spot, {
+        foreignKey: 'ownerId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   User.init({
