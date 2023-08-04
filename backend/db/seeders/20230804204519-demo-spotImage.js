f76b7195f6477658955404fc2a7f17f165fccc35
Kyle Mooney <kjmooney27@gmail.com>
2023-08-04 16:49:45 -0400

    spotImages created

---
+++ backend/db/seeders/20230804204519-demo-spotImage.js
@@ -0,0 +1,54 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   options.tableName = 'SpotImages';
+   return queryInterface.bulkInsert(options, [
+    {
+      spotId: 1,
+      url: 'https://www.google.com',
+      previewImage: false
+    },
+    {
+      spotId: 2,
+      url: 'https://www.google.com',
+      previewImage: false
+    },
+    {
+      spotId: 3,
+      url: 'https://www.google.com',
+      previewImage: false
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {})
+  }
+};
