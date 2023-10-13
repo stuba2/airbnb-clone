09974eec850fb1094fb4f407fbc4e509e6e590e3
Kyle Mooney <kjmooney27@gmail.com>
2023-10-13 00:10:12 -0400

    nav + profile button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/signupFormPage";
 import * as sessionActions from './store/session'
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch()
@@ -14,16 +15,19 @@ function App() {
   }, [dispatch])
 
   return (
-    isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/singup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )
+    <div>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/singup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </div>
   );
 }
 
