4dd329b215cba3dcb01ed68ade4607b3cac34e28
Kyle Mooney <kjmooney27@gmail.com>
2023-10-28 00:44:58 -0400

    added in console.logs for prod debugging

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -10,6 +10,7 @@ const EDIT_SPOT = "spot/edit"
 const DELETE_SPOT = "spot/delete"
 
 const getSpots = (spots) => {
+  console.log('getSpots action: payload (spots): ', spots)
   return {
     type: GET_SPOTS,
     payload: spots
@@ -17,6 +18,7 @@ const getSpots = (spots) => {
 }
 
 const getASpot = (spot) => {
+  console.log('getASpot action: payload (spot): ', spot)
   return {
     type: GET_SPOT,
     payload: spot
@@ -24,6 +26,7 @@ const getASpot = (spot) => {
 }
 
 const findOwner = (data) => {
+  console.log('findOwner action: payload (data): ', data)
   return {
     type: FIND_OWNER,
     payload: data
@@ -31,6 +34,7 @@ const findOwner = (data) => {
 }
 
 const createASpot = (spot) => {
+  console.log('createASpot action: payload (spot): ', spot)
   return {
     type: POST_SPOT,
     payload: spot
@@ -38,6 +42,7 @@ const createASpot = (spot) => {
 }
 
 const addImage = (data) => {
+  console.log('addImage action: payload (data): ', data)
   return {
     type: ADD_IMAGE,
     payload: data
@@ -45,6 +50,7 @@ const addImage = (data) => {
 }
 
 const getOwnedSpots = (data) => {
+  console.log('getOwnedSpots action: payload (data): ', data)
   return {
     type: OWNED_SPOTS,
     payload: data
@@ -66,10 +72,14 @@ const deleteSpot = (data) => {
 }
 
 export const getSpotsThunk = () => async (dispatch) => {
+  console.log('in getSpotsThunk')
   const response = await csrfFetch("/api/spots")
+  console.log('getSpotsThunk: response: ', response)
 
   if (response.ok) {
+    console.log('in getSpotsThunk if (response.ok) block')
     const data = await response.json()
+    console.log('getSpotsThunk if block: data: ', data)
     dispatch(getSpots(data))
     return data
   } else {
@@ -78,10 +88,15 @@ export const getSpotsThunk = () => async (dispatch) => {
 }
 
 export const getASpotThunk = (spot) => async (dispatch) => {
+  console.log('in getASpotThunk')
+  console.log('getASpotThunk: passed in spot and typeof: ', spot, typeof spot)
   const response = await csrfFetch(`/api/spots/${spot.id}`)
+  console.log('getASpotThunk: response: ', response)
 
   if (response.ok) {
+    console.log('in getASpotThunk if (response.ok) block')
     const data = await response.json()
+    console.log('getASpotThunk if block: data: ', data)
     dispatch(getASpot(data))
     return data
   } else {
@@ -90,10 +105,15 @@ export const getASpotThunk = (spot) => async (dispatch) => {
 }
 
 export const getSpotDeetsThunk = (id) => async (dispatch) => {
+  console.log('in getSpotDeetsThunk')
+  console.log('getSpotDeetsThunk: passed in id and typeof: ', id, typeof id)
   const response = await csrfFetch(`/api/spots/${id}`)
+  console.log('getSpotDeetsThunk: response: ', response)
 
   if (response.ok) {
+    console.log('in getSpotDeetsThunk if (response.ok) block: ')
     const data = await response.json()
+    console.log('getSpotDeetsThunk if block: data: ', data)
     dispatch(findOwner(data))
     return data
   } else {
@@ -102,7 +122,10 @@ export const getSpotDeetsThunk = (id) => async (dispatch) => {
 }
 
 export const createSpotThunk = (spotForm) => async (dispatch) => {
+  console.log('in createSpotThunk')
+  console.log('createSpotThunk: passed in spotForm and typeof: ', spotForm, typeof spotForm)
   try {
+    console.log('in createSpotThunk try block')
     const newSpot = await csrfFetch('/api/spots/', {
       method: "POST",
       headers: {
@@ -110,13 +133,17 @@ export const createSpotThunk = (spotForm) => async (dispatch) => {
       },
       body: JSON.stringify(spotForm)
     })
+    console.log('createSpotThunk: newSpot: ', newSpot)
 
     // if (newSpot.ok)
       const createdSpot = await newSpot.json()
+      console.log('createSpotThunk: createdSpot: ', createdSpot)
       dispatch(createASpot(createdSpot))
       return createdSpot
 
   } catch (error) {
+    console.log('in createSpotThunk catch block')
+    console.log('createSpotThunk: error: ', error)
     return error
   }
 
@@ -126,6 +153,10 @@ export const createSpotThunk = (spotForm) => async (dispatch) => {
 }
 
 export const addImageThunk = (newSpotId, imageObj) => async (dispatch) => {
+  console.log('in addImageThunk')
+  console.log('addImageThunk: passed in newSpotId and typeof: ', newSpotId, typeof newSpotId)
+  console.log('addImageThunk: passed in +newSpotId and typeof: ', +newSpotId, typeof +newSpotId)
+  console.log('addImageThunk: passed in imageObj and typeof: ', imageObj, typeof imageObj)
   const response = await csrfFetch(`/api/spots/${+newSpotId}/images/`, {
     method: "POST",
     headers: {
@@ -133,11 +164,15 @@ export const addImageThunk = (newSpotId, imageObj) => async (dispatch) => {
     },
     body: JSON.stringify(imageObj)
   })
+  console.log('addImageThunk: response: ', response)
 
   const updatedSpot = await csrfFetch(`/api/spots/${+newSpotId}/`)
+  console.log('addImageThunk: updatedSpot: ', updatedSpot)
 
   if (response.ok && updatedSpot.ok) {
+    console.log('addImageThunk if (response.ok && updatedSpot.ok) block')
     const spotUpdate = await updatedSpot.json()
+    console.log('addImageThunk: spotUpdate: ', spotUpdate)
     dispatch(addImage(spotUpdate))
     return spotUpdate
   } else {
@@ -156,6 +191,10 @@ export const getOwnedSpotsThunk = (ownerId) => async (dispatch) => {
 }
 
 export const editASpotThunk = (spotForm, spotId) => async (dispatch) => {
+  console.log('in editASpotThunk')
+  console.log('editASpotThunk: passed in spotForm and typeof: ', spotForm, typeof spotForm)
+  console.log('editASpotThunk: passed in spotId and typeof: ', spotId, typeof spotId)
+  console.log('editASpotThunk: passed in +spotId and typeof: ', +spotId, typeof +spotId)
   const response = await csrfFetch(`/api/spots/${spotId}`, {
     method: "PUT",
     headers: {
@@ -163,11 +202,15 @@ export const editASpotThunk = (spotForm, spotId) => async (dispatch) => {
     },
     body: JSON.stringify(spotForm)
   })
+  console.log('editASpotThunk: response (uninvoked): ', response)
 
   const secondResponse = await csrfFetch(`/api/spots/${spotId}`)
+  console.log('editASpotThunk: secondResponse: ', secondResponse)
 
   if (secondResponse.ok) {
+    console.log('editASpotThunk if (secondResponse.ok) block')
     const updatedSpot = await secondResponse.json()
+    console.log('editASpotThunk if block: updatedSpot: ', updatedSpot)
     dispatch(findOwner(updatedSpot))
     return updatedSpot
   } else {
@@ -194,8 +237,11 @@ const spotReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_SPOTS:
       newState = {...state}
+      console.log('spotReducer GET_SPOTS: previous state: ', newState)
       const getArr = action.payload.Spots
+      console.log('spotReducer GET_SPOTS: action (action.payload.Spots) getArr: ', getArr)
       getArr.map((spotObj) => newState[spotObj.id] = spotObj)
+      console.log('spotReducer GET_SPOTS: new state: ', newState)
       return newState
     case FIND_OWNER:
       newState = {...state}
@@ -204,18 +250,27 @@ const spotReducer = (state = initialState, action) => {
       return newState
     case POST_SPOT:
       newState = {...state}
+      console.log('spotReducer POST_SPOT: previous state: ', newState)
       const newSpot = action.payload
+      console.log('spotReducer POST_SPOT: action (action.payload) newSpot: ', newSpot)
       newState[newSpot.id] = newSpot
+      console.log('spotReducer POST_SPOT: new state: ', newState)
       return newState
     case ADD_IMAGE:
       newState = {...state}
+      console.log('spotReducer ADD_IMAGE: previous state: ', newState)
       const imgData = action.payload
+      console.log('spotReducer ADD_IMAGE: action (action.payload) imgData: ', imgData)
       newState[imgData.id] = imgData
+      console.log('spotReducer ADD_IMAGE: new state: ', newState)
       return newState
     case EDIT_SPOT:
       newState = {...state}
+      console.log('spotReducer ADD_IMAGE: previous state: ', newState)
       const spot = action.payload
+      console.log('spotReducer ADD_IMAGE: action (action.payload) spot: ', spot)
       newState[spot.id] = spot
+      console.log('spotReducer ADD_IMAGE: new state: ', newState)
       return newState
     case DELETE_SPOT:
       newState = {...state}
